dcf8cd565fe63086ab9192a03d95fa889ba707b1
Mark Norton <youthman2002@gmail.com>
2019-10-16 23:20:37 -0400

    Tabs Working - Tab1 Recycler Working

---
+++ app/src/main/java/com/marknorton/openclassroomsnytapp/ArticleAdapter.kt
@@ -0,0 +1,38 @@
+package com.marknorton.openclassroomsnytapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ *  Created by Mark Norton on 10/16/2019.
+ *
+ */
+
+  class ArticleAdapter(val articleList: ArrayList<ArticleModel>): RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.story_row_layout, parent, false)
+        return ViewHolder(v)
+
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.tvSection?.text = articleList[position].section
+        holder?.imageView2?.text = articleList[position].imageURL
+        holder?.tvHeadline?.text = articleList[position].headline
+        holder?.tvDate?.text = articleList[position].date
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val tvSection: TextView = itemView.findViewById<TextView>(R.id.tvSection)
+        val imageView2: TextView = itemView.findViewById<TextView>(R.id.imageView2)
+        val tvHeadline: TextView = itemView.findViewById<TextView>(R.id.tvHeadline)
+        val tvDate: TextView = itemView.findViewById<TextView>(R.id.tvDate)
+    }
+}
--- app/src/main/java/com/marknorton/openclassroomsnytapp/ui/TopStoriesFragment.kt
+++ app/src/main/java/com/marknorton/openclassroomsnytapp/ui/TopStoriesFragment.kt
@@ -2,29 +2,40 @@ package com.marknorton.openclassroomsnytapp.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.marknorton.openclassroomsnytapp.ArticleAdapter
+import com.marknorton.openclassroomsnytapp.ArticleModel
 import com.marknorton.openclassroomsnytapp.R
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
 class TopStoriesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-//    private var listener: OnFragmentInteractionListener? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        inflater.inflate(R.layout.fragment_top_stories, container, false)!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //     inflater.inflate(R.layout.fragment_top_stories, container, false)
+
+       val list = ArrayList<ArticleModel>()
+        list.add(ArticleModel("Section1","Image1","headline1","date1"))
+        list.add(ArticleModel("Section2","Image2","headline2","date2"))
+        list.add(ArticleModel("Section3","Image3","headline3","date3"))
+        list.add(ArticleModel("Section4","Image4","headline4","date4"))
+
+        val rootView = inflater.inflate(R.layout.fragment_top_stories, container, false)
+        val recyclerview = rootView.findViewById(R.id.rvTopStories) as RecyclerView
+        recyclerview.layoutManager = LinearLayoutManager(activity)
+        recyclerview.adapter = ArticleAdapter(list)
+        return rootView
+
+    }
+
 }
 
