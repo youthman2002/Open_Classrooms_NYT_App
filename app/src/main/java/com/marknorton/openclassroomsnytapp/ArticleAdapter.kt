88883636554c0129b5cbda99966ae05bd1f5c0a5
Mark Norton <youthman2002@gmail.com>
2019-11-27 15:07:53 -0500

    Tabs Working - Tab1 & Tab2 Recycler Working - Top Stories Images Working - Images and NoImageAvailable working-OnClick working - URL Onclick Working - Search Results placeholder working - Top Stories Working - Most Popular Working - Menu Created - Working on Notification Screen - Notifications are saving in Shared Preferences - Search Category working - Search Date Spinners Working - Next create Search Results Page - Search and Search Results now working - Spinners need to be updated to DatePicker - Back Button Added to all pages on ActionBar - About and Help Pages Updated - onClickListener set to CardView instead of individual links on elemets

--- app/src/main/java/com/marknorton/openclassroomsnytapp/ArticleAdapter.kt
+++ app/src/main/java/com/marknorton/openclassroomsnytapp/ArticleAdapter.kt
@@ -3,12 +3,12 @@ package com.marknorton.openclassroomsnytapp
 // import com.bumptech.glide.Glide
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -21,6 +21,8 @@ import com.marknorton.openclassroomsnytapp.ui.WebViewActivity
  */
 
   class ArticleAdapter(val articleList: ArrayList<ArticleModel>, context: Context): RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
+
+// class ArticleAdapter (val articleList: ArrayList<ArticleModel>, context: Context, val clickListener: (PartData) -> Unit) : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
 
     private val context: Context = context
 
@@ -36,17 +38,20 @@ import com.marknorton.openclassroomsnytapp.ui.WebViewActivity
 //        holder.imageView.image = articleList[position].imageURL
         holder.tvHeadline.text = articleList[position].headline
         holder.tvDate.text = articleList[position].date
+//        holder.storyRow = articleList[position].url
 
-        holder.tvSection.setOnClickListener{
+        holder.storyRow.setOnClickListener{
             val intent = Intent(context, WebViewActivity::class.java)
-    //        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             intent.putExtra("url", articleList[position].url)
             context!!.startActivity(intent)
-
-            Log.d("Log","Log-ClickedURL:SECTION            "+articleList[position].url)
-//            val intent = Intent(this.Context, WebViewActivity::class.java)
-//            startActivity(intent)
-
+//            Log.d("Log","Log-ClickedURL:SECTION            "+articleList[position].url)
+        }
+ /*
+        holder.tvSection.setOnClickListener{
+            val intent = Intent(context, WebViewActivity::class.java)
+            intent.putExtra("url", articleList[position].url)
+            context!!.startActivity(intent)
+//            Log.d("Log","Log-ClickedURL:SECTION            "+articleList[position].url)
         }
         holder.tvHeadline.setOnClickListener{
             Log.d("Log","Log-ClickedURL:HEADLINE            "+articleList[position].url)
@@ -57,8 +62,9 @@ import com.marknorton.openclassroomsnytapp.ui.WebViewActivity
         holder.imageView.setOnClickListener{
             Log.d("Log","Log-ClickedURL:IMAGE            "+articleList[position].url)
         }
-
         Log.d("Log","Log-ADAPTERIMAGE:            "+(articleList[position].imageURL))
+   */
+
         Glide.with(holder.imageView)
             .load(articleList[position].imageURL)
             .apply(
@@ -78,6 +84,7 @@ import com.marknorton.openclassroomsnytapp.ui.WebViewActivity
         val imageView: ImageView = itemView.findViewById(R.id.imageView)
         val tvHeadline: TextView = itemView.findViewById(R.id.tvHeadline)
         val tvDate: TextView = itemView.findViewById(R.id.tvDate)
+        val storyRow: CardView = itemView.findViewById(R.id.storyRow)
 
 //        var myAdapter = ArticleAdapter(ArticleAdapter)
 
